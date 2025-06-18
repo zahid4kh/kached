d59f8d9841aabcc5385251abbc5fa87ee99f5927
zahid4kh <halilzahid@gmail.com>
2025-06-19 00:23:04 +0200

    .

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+@file:JvmName("Kached")
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import theme.AppTheme
+import java.awt.Dimension
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.getKoin
+import kached.resources.*
+
+
+fun main() = application {
+    startKoin {
+        modules(appModule)
+    }
+
+    val viewModel = getKoin().get<MainViewModel>()
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(size = DpSize(800.dp, 600.dp)),
+        alwaysOnTop = true,
+        title = "Kached - Made with Compose for Desktop Wizard",
+        icon = null
+    ) {
+        window.minimumSize = Dimension(800, 600)
+
+        AppTheme {
+            App(
+                viewModel = viewModel
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/UiState.kt
@@ -0,0 +1,4 @@
+data class UiState(
+    val darkMode: Boolean = false,
+    val isConverting: Boolean = false
+)
