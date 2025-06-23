fdcac442b6f00f58b2253187527a8997abc95ee0
zahid4kh <halilzahid@gmail.com>
2025-06-23 19:16:01 +0200

    extending ViewModel() from precompose; added addSnippet() and removeSnippet() methods

--- src/main/kotlin/MainViewModel.kt
+++ src/main/kotlin/MainViewModel.kt
@@ -1,24 +1,48 @@
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import moe.tlaster.precompose.viewmodel.ViewModel
+import moe.tlaster.precompose.viewmodel.viewModelScope
 
 class MainViewModel(
     private val database: Database,
-) {
+): ViewModel() {
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
 
-    private val scope = CoroutineScope(Dispatchers.Main)
+    private val _snippets = MutableStateFlow<List<Snippet>>(emptyList())
+    val snippets: StateFlow<List<Snippet>> = _snippets.asStateFlow()
+
+
+    private val scope = viewModelScope
 
     init {
-        scope.launch {
+        scope.launch(Dispatchers.IO) {
             val settings = database.getSettings()
             _uiState.value = _uiState.value.copy(
                 darkMode = settings.darkMode,
             )
+            val loadedSnippets = database.getSnippets()
+            _snippets.value = loadedSnippets
+        }
+    }
+
+    fun addSnippet(newSnippet: Snippet) {
+        val newSnippets = _snippets.value + newSnippet
+        _snippets.value = newSnippets
+
+        scope.launch(Dispatchers.IO) {
+            database.saveSnippets((newSnippets))
+        }
+    }
+
+    fun removeSnippet(snippetTitleToRemove: String) {
+        val updatedSnippets = _snippets.value.filter{it.title != snippetTitleToRemove}
+        _snippets.value = updatedSnippets
+        scope.launch(Dispatchers.IO){
+            database.saveSnippets(updatedSnippets)
         }
     }
 
@@ -26,7 +50,7 @@ class MainViewModel(
         val newDarkMode = !_uiState.value.darkMode
         _uiState.value = _uiState.value.copy(darkMode = newDarkMode)
 
-        scope.launch {
+        scope.launch(Dispatchers.IO) {
             val settings = database.getSettings()
             database.saveSettings(settings.copy(darkMode = newDarkMode))
         }
