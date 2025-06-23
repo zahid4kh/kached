0fa5a8ba7ad602cb6b718d1e12e68350313e0d67
zahid4kh <halilzahid@gmail.com>
2025-06-23 20:23:18 +0200

    main App() with navigation

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,30 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import moe.tlaster.precompose.PreComposeApp
+import moe.tlaster.precompose.navigation.NavHost
+import moe.tlaster.precompose.navigation.Navigator
+import moe.tlaster.precompose.navigation.rememberNavigator
 import theme.AppTheme
 
 
@@ -17,21 +34,22 @@ fun App(
     viewModel: MainViewModel
 ) {
     val uiState by viewModel.uiState.collectAsState()
-    AppTheme(darkTheme = uiState.darkMode) {
-        val secondSnippet = Snippet(
-            title = "Second title",
-            description = "kached app sfvhndfvjdfv",
-            code = "hello world"
-        )
-        val firstSnippet = Snippet(
-            title = "First title",
-            description = "dfvbjdfnvsdmfkvknnb",
-            code = "fjvnjdfvnjmdfkvfvdv"
-        )
-
-
-        viewModel.removeSnippet(secondSnippet.title)
-        viewModel.removeSnippet(firstSnippet.title)
+    PreComposeApp {
+        AppTheme(darkTheme = uiState.darkMode) {
+            val navigator = rememberNavigator()
+            Surface {
+                NavHost(navigator = navigator, initialRoute = "/main") {
+                    scene("/main") {
+                        MainScreen(navigator = navigator, viewModel = viewModel)
+                    }
+                    scene("/add_snippet") {
+                        AddSnippetScreen(navigator = navigator, viewModel = viewModel)
+                    }
+                    scene("/snippets") {
+                        SnippetsScreen(navigator = navigator, viewModel = viewModel)
+                    }
+                }
+            }
+        }
     }
 }
-
