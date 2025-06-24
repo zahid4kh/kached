731289837be0a6f80e8f06b265094ca98cf66a6d
zahid4kh <halilzahid@gmail.com>
2025-06-24 09:53:28 +0200

    AddSnippetScreen.kt -> moving focus down on TAB; added animateContentSize()

--- src/main/kotlin/screens/AddSnippetScreen.kt
+++ src/main/kotlin/screens/AddSnippetScreen.kt
@@ -1,3 +1,5 @@
+import androidx.compose.animation.animateBounds
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,7 +11,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -30,7 +36,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.layout.LookaheadScope
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PlatformImeOptions
 import androidx.compose.ui.unit.dp
 import kached.resources.Res
 import kached.resources.move_left
@@ -43,6 +59,8 @@ fun AddSnippetScreen(navigator: Navigator, viewModel: MainViewModel) {
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
     var code by remember { mutableStateOf("") }
+
+    val focusManager = LocalFocusManager.current
 
     val textFieldShape = MaterialTheme.shapes.medium
 
@@ -72,6 +90,7 @@ fun AddSnippetScreen(navigator: Navigator, viewModel: MainViewModel) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .background(MaterialTheme.colorScheme.secondaryContainer)
                 .padding(paddingValues)
                 .padding(16.dp)
                 .verticalScroll(rememberScrollState()),
@@ -82,9 +101,17 @@ fun AddSnippetScreen(navigator: Navigator, viewModel: MainViewModel) {
                 value = title,
                 onValueChange = { title = it },
                 label = { Text("Title") },
-                modifier = Modifier.fillMaxWidth(0.8f),
-                singleLine = true,
-                shape = RoundedCornerShape(8.dp)
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .onPreviewKeyEvent {
+                        if (it.type == KeyEventType.KeyUp && it.key == Key.Tab) {
+                            focusManager.moveFocus(FocusDirection.Down)
+                            true
+                        } else {
+                            false
+                        }
+                    },
+                shape = textFieldShape
             )
             OutlinedTextField(
                 value = description,
@@ -92,9 +119,18 @@ fun AddSnippetScreen(navigator: Navigator, viewModel: MainViewModel) {
                 label = { Text("Description") },
                 modifier = Modifier
                     .fillMaxWidth(0.8f)
-                    .heightIn(min = 80.dp),
+                    .heightIn(min = 80.dp)
+                    .animateContentSize()
+                    .onPreviewKeyEvent {
+                        if (it.type == KeyEventType.KeyUp && it.key == Key.Tab) {
+                            focusManager.moveFocus(FocusDirection.Down)
+                            true
+                        } else {
+                            false
+                        }
+                    },
                 maxLines = 5,
-                shape = RoundedCornerShape(8.dp)
+                shape = textFieldShape
             )
             OutlinedTextField(
                 value = code,
@@ -102,9 +138,10 @@ fun AddSnippetScreen(navigator: Navigator, viewModel: MainViewModel) {
                 label = { Text("Code") },
                 modifier = Modifier
                     .fillMaxWidth(0.8f)
-                    .heightIn(min = 150.dp),
+                    .heightIn(min = 150.dp)
+                    .animateContentSize(),
                 maxLines = 10,
-                shape = RoundedCornerShape(8.dp)
+                shape = textFieldShape
             )
 
             Spacer(Modifier.height(20.dp))
@@ -116,11 +153,11 @@ fun AddSnippetScreen(navigator: Navigator, viewModel: MainViewModel) {
             ) {
                 Button(
                     onClick = { navigator.popBackStack() },
-                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.errorContainer),
                     shape = RoundedCornerShape(8.dp),
                     modifier = Modifier.weight(1f).height(50.dp)
                 ) {
-                    Text("Cancel", color = MaterialTheme.colorScheme.onSecondary)
+                    Text("Cancel", color = MaterialTheme.colorScheme.onErrorContainer)
                 }
                 Spacer(Modifier.width(16.dp))
                 Button(
