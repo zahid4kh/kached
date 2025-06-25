7dc01c6db878227634c0ecd70be2167f8176d98f
zahid4kh <halilzahid@gmail.com>
2025-06-25 22:16:34 +0200

    moved opened snipped dialog to a separate file

--- src/main/kotlin/screens/SnippetsScreen.kt
+++ src/main/kotlin/screens/SnippetsScreen.kt
@@ -1,7 +1,7 @@
-import androidx.compose.animation.AnimatedVisibility
+
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.*
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -9,26 +9,15 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import deskit.dialogs.InfoDialog
 import deskit.dialogs.file.FileSaverDialog
-import dev.snipme.highlights.Highlights
-import dev.snipme.highlights.model.SyntaxLanguage
-import dev.snipme.highlights.model.SyntaxThemes
-import kached.resources.Res
-import kached.resources.copy
-import kached.resources.square_m
 import moe.tlaster.precompose.navigation.Navigator
-import org.jetbrains.compose.resources.painterResource
-import theme.getJetbrainsMonoFamily
+import screens.ExpandedSnippet
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -89,136 +78,10 @@ fun SnippetsScreen(navigator: Navigator, viewModel: MainViewModel) {
                 }
             }
             uiState.expandedSnippet?.let{openedSnippet->
-                InfoDialog(
-                    width = 1200.dp,
-                    height = 1000.dp,
-                    title = openedSnippet.title,
-                    message = openedSnippet.description?:"",
-                    onClose = { viewModel.collapseSnippet() },
-                    content = {
-                        Column{
-                            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End){
-                                // copy icon buttons
-                                Row(verticalAlignment = Alignment.CenterVertically,
-                                    horizontalArrangement = Arrangement.spacedBy(5.dp)){
-                                    AnimatedVisibility(visible = uiState.codeCopiedAsText == true){
-                                        Box(modifier = Modifier
-                                            .border(width = 1.dp, color = Color.Green.copy(green = 100f), shape = MaterialTheme.shapes.medium)){
-                                            Text("Copied as text!", modifier = Modifier.padding(5.dp),
-                                                style = MaterialTheme.typography.bodyMedium)
-                                        }
-                                    }
-                                    AnimatedVisibility(visible = uiState.codeCopiedAsText == false){
-                                        Box(modifier = Modifier
-                                            .border(width = 1.dp, color = Color.Red.copy(red = 170f), shape = MaterialTheme.shapes.medium)){
-                                            Text("Failed to copy as text!", modifier = Modifier.padding(5.dp),
-                                                style = MaterialTheme.typography.bodyMedium)
-                                        }
-                                    }
-                                    // copy as plain-text
-                                    TooltipBox(
-                                        positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
-                                        tooltip = {
-                                            RichTooltip(
-                                                title = {
-                                                    Text(
-                                                        text = "Copy as text",
-                                                        style = MaterialTheme.typography.titleMedium.copy(
-                                                            fontWeight = FontWeight.Bold
-                                                        )
-                                                    )
-                                                },
-                                                caretSize = DpSize(27.dp, 17.dp),
-                                                colors = RichTooltipColors(
-                                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                                                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                                    actionContentColor = MaterialTheme.colorScheme.onTertiaryContainer
-                                                )
-                                            ) {
-                                                Text("Code will be copied as plain text")
-                                            }
-                                        },
-                                        state = rememberTooltipState()
-                                    ){
-                                        IconButton(onClick = {viewModel.copyCodeAsPlainText(openedSnippet)}){
-                                            Icon(
-                                                painterResource(Res.drawable.copy),
-                                                contentDescription = "Copy code as plaintext"
-                                            )
-                                        }
-                                    }
-
-                                    AnimatedVisibility(visible = uiState.codeCopiedAsMarkdown == true){
-                                        Box(modifier = Modifier
-                                            .border(width = 1.dp, color = Color.Green.copy(green = 100f), shape = MaterialTheme.shapes.medium)){
-                                            Text("Copied as markdown!", modifier = Modifier.padding(5.dp),
-                                                style = MaterialTheme.typography.bodyMedium)
-                                        }
-                                    }
-                                    AnimatedVisibility(visible = uiState.codeCopiedAsText == false){
-                                        Box(modifier = Modifier
-                                            .border(width = 1.dp, color = Color.Red.copy(red = 170f), shape = MaterialTheme.shapes.medium)){
-                                            Text("Failed to copy as markdown!", modifier = Modifier.padding(5.dp),
-                                                style = MaterialTheme.typography.bodyMedium)
-                                        }
-                                    }
-
-                                    // copy as markdown codeblock
-                                    TooltipBox(
-                                        positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
-                                        tooltip = {
-                                            RichTooltip(
-                                                title = {
-                                                    Text(
-                                                        text = "Copy as markdown",
-                                                        style = MaterialTheme.typography.titleMedium.copy(
-                                                            fontWeight = FontWeight.Bold
-                                                        )
-                                                    )
-                                                },
-                                                caretSize = DpSize(27.dp, 17.dp),
-                                                colors = RichTooltipColors(
-                                                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                                                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                                    titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                                    actionContentColor = MaterialTheme.colorScheme.onTertiaryContainer
-                                                )
-                                            ) {
-                                                Text("Code will be copied as markdown codeblock")
-                                            }
-                                        },
-                                        state = rememberTooltipState()
-                                    ){
-                                        IconButton(onClick = {viewModel.copyCodeAsMarkdown(openedSnippet)}){
-                                            Icon(
-                                                painterResource(Res.drawable.square_m),
-                                                contentDescription = "Copy code as a markdown codeblock"
-                                            )
-                                        }
-                                    }
-
-                                }
-                            }
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .clip(MaterialTheme.shapes.medium)
-                                    .background(MaterialTheme.colorScheme.tertiaryContainer.copy(alpha = 0.6f))
-                                    .padding(12.dp)
-                            ) {
-                                val code = openedSnippet.code ?: "// No code here"
-                                val highlights = remember(code) {
-                                    Highlights.Builder()
-                                        .code(code)
-                                        .language(SyntaxLanguage.KOTLIN) // will make this dynamic
-                                        .theme(SyntaxThemes.atom()) // will make this dynamic
-                                        .build()
-                                }
-                                ViewCodeText(highlights = highlights) // edited version of CodeTextView()
-                            }
-                        }
-                    }
+                ExpandedSnippet(
+                    uiState = uiState,
+                    viewModel = viewModel,
+                    openedSnippet = openedSnippet
                 )
             }
 
@@ -252,38 +115,5 @@ fun SnippetsScreen(navigator: Navigator, viewModel: MainViewModel) {
                 )
             }
         }
-    }
-}
-
-/*
-copied from CodeTextView() from `package dev.snipme.kodeview.view`
-had to add the `fontFamily = getJetbrainsMonoFamily()`
-*/
-@Composable
-private fun ViewCodeText(
-    modifier: Modifier = Modifier.background(Color.Transparent),
-    highlights: Highlights
-) {
-    var textState by remember {
-        mutableStateOf(AnnotatedString(highlights.getCode()))
-    }
-
-    LaunchedEffect(highlights) {
-        textState = highlights
-            .getHighlights()
-            .generateAnnotatedString(highlights.getCode())
-    }
-
-    Surface(
-        modifier = modifier,
-        color = Color.Transparent
-    ) {
-        Text(
-            modifier = modifier
-                .verticalScroll(rememberScrollState())
-                .horizontalScroll(rememberScrollState()),
-            text = textState,
-            fontFamily = getJetbrainsMonoFamily()
-        )
     }
 }
