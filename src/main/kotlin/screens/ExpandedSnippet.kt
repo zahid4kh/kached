15324b29b958c361b1d088827085f19dfc20f398
zahid4kh <halilzahid@gmail.com>
2025-06-25 22:24:17 +0200

    added selected language to the top of code content

--- src/main/kotlin/screens/ExpandedSnippet.kt
+++ src/main/kotlin/screens/ExpandedSnippet.kt
@@ -44,105 +44,116 @@ fun ExpandedSnippet(
             Column{
                 Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End){
                     // copy icon buttons
-                    Row(verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(5.dp)){
-                        AnimatedVisibility(visible = uiState.codeCopiedAsText == true){
-                            Box(modifier = Modifier
-                                .border(width = 1.dp, color = Color.Green.copy(green = 100f), shape = MaterialTheme.shapes.medium)){
-                                Text("Copied as text!", modifier = Modifier.padding(5.dp),
-                                    style = MaterialTheme.typography.bodyMedium)
-                            }
-                        }
-                        AnimatedVisibility(visible = uiState.codeCopiedAsText == false){
-                            Box(modifier = Modifier
-                                .border(width = 1.dp, color = Color.Red.copy(red = 170f), shape = MaterialTheme.shapes.medium)){
-                                Text("Failed to copy as text!", modifier = Modifier.padding(5.dp),
-                                    style = MaterialTheme.typography.bodyMedium)
-                            }
-                        }
-                        // copy as plain-text
-                        TooltipBox(
-                            positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
-                            tooltip = {
-                                RichTooltip(
-                                    title = {
-                                        Text(
-                                            text = "Copy as text",
-                                            style = MaterialTheme.typography.titleMedium.copy(
-                                                fontWeight = FontWeight.Bold
-                                            )
-                                        )
-                                    },
-                                    caretSize = DpSize(27.dp, 17.dp),
-                                    colors = RichTooltipColors(
-                                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                        titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                        actionContentColor = MaterialTheme.colorScheme.onTertiaryContainer
-                                    )
-                                ) {
-                                    Text("Code will be copied as plain text")
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(
+                            openedSnippet.language.toString().lowercase(),
+                            fontFamily = getJetbrainsMonoFamily(),
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+                        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(5.dp)){
+                            AnimatedVisibility(visible = uiState.codeCopiedAsText == true){
+                                Box(modifier = Modifier
+                                    .border(width = 1.dp, color = Color.Green.copy(green = 100f), shape = MaterialTheme.shapes.medium)){
+                                    Text("Copied as text!", modifier = Modifier.padding(5.dp),
+                                        style = MaterialTheme.typography.bodyMedium)
                                 }
-                            },
-                            state = rememberTooltipState()
-                        ){
-                            IconButton(onClick = {viewModel.copyCodeAsPlainText(openedSnippet)}){
-                                Icon(
-                                    painterResource(Res.drawable.copy),
-                                    contentDescription = "Copy code as plaintext"
-                                )
                             }
-                        }
-
-                        AnimatedVisibility(visible = uiState.codeCopiedAsMarkdown == true){
-                            Box(modifier = Modifier
-                                .border(width = 1.dp, color = Color.Green.copy(green = 100f), shape = MaterialTheme.shapes.medium)){
-                                Text("Copied as markdown!", modifier = Modifier.padding(5.dp),
-                                    style = MaterialTheme.typography.bodyMedium)
-                            }
-                        }
-                        AnimatedVisibility(visible = uiState.codeCopiedAsText == false){
-                            Box(modifier = Modifier
-                                .border(width = 1.dp, color = Color.Red.copy(red = 170f), shape = MaterialTheme.shapes.medium)){
-                                Text("Failed to copy as markdown!", modifier = Modifier.padding(5.dp),
-                                    style = MaterialTheme.typography.bodyMedium)
-                            }
-                        }
-
-                        // copy as markdown codeblock
-                        TooltipBox(
-                            positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
-                            tooltip = {
-                                RichTooltip(
-                                    title = {
-                                        Text(
-                                            text = "Copy as markdown",
-                                            style = MaterialTheme.typography.titleMedium.copy(
-                                                fontWeight = FontWeight.Bold
-                                            )
-                                        )
-                                    },
-                                    caretSize = DpSize(27.dp, 17.dp),
-                                    colors = RichTooltipColors(
-                                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
-                                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                        titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                                        actionContentColor = MaterialTheme.colorScheme.onTertiaryContainer
-                                    )
-                                ) {
-                                    Text("Code will be copied as markdown codeblock")
+                            AnimatedVisibility(visible = uiState.codeCopiedAsText == false){
+                                Box(modifier = Modifier
+                                    .border(width = 1.dp, color = Color.Red.copy(red = 170f), shape = MaterialTheme.shapes.medium)){
+                                    Text("Failed to copy as text!", modifier = Modifier.padding(5.dp),
+                                        style = MaterialTheme.typography.bodyMedium)
                                 }
-                            },
-                            state = rememberTooltipState()
-                        ){
-                            IconButton(onClick = {viewModel.copyCodeAsMarkdown(openedSnippet)}){
-                                Icon(
-                                    painterResource(Res.drawable.square_m),
-                                    contentDescription = "Copy code as a markdown codeblock"
-                                )
                             }
-                        }
+                            // copy as plain-text
+                            TooltipBox(
+                                positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+                                tooltip = {
+                                    RichTooltip(
+                                        title = {
+                                            Text(
+                                                text = "Copy as text",
+                                                style = MaterialTheme.typography.titleMedium.copy(
+                                                    fontWeight = FontWeight.Bold
+                                                )
+                                            )
+                                        },
+                                        caretSize = DpSize(27.dp, 17.dp),
+                                        colors = RichTooltipColors(
+                                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                            contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                            titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                            actionContentColor = MaterialTheme.colorScheme.onTertiaryContainer
+                                        )
+                                    ) {
+                                        Text("Code will be copied as plain text")
+                                    }
+                                },
+                                state = rememberTooltipState()
+                            ){
+                                IconButton(onClick = {viewModel.copyCodeAsPlainText(openedSnippet)}){
+                                    Icon(
+                                        painterResource(Res.drawable.copy),
+                                        contentDescription = "Copy code as plaintext"
+                                    )
+                                }
+                            }
 
+                            AnimatedVisibility(visible = uiState.codeCopiedAsMarkdown == true){
+                                Box(modifier = Modifier
+                                    .border(width = 1.dp, color = Color.Green.copy(green = 100f), shape = MaterialTheme.shapes.medium)){
+                                    Text("Copied as markdown!", modifier = Modifier.padding(5.dp),
+                                        style = MaterialTheme.typography.bodyMedium)
+                                }
+                            }
+                            AnimatedVisibility(visible = uiState.codeCopiedAsText == false){
+                                Box(modifier = Modifier
+                                    .border(width = 1.dp, color = Color.Red.copy(red = 170f), shape = MaterialTheme.shapes.medium)){
+                                    Text("Failed to copy as markdown!", modifier = Modifier.padding(5.dp),
+                                        style = MaterialTheme.typography.bodyMedium)
+                                }
+                            }
+
+                            // copy as markdown codeblock
+                            TooltipBox(
+                                positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+                                tooltip = {
+                                    RichTooltip(
+                                        title = {
+                                            Text(
+                                                text = "Copy as markdown",
+                                                style = MaterialTheme.typography.titleMedium.copy(
+                                                    fontWeight = FontWeight.Bold
+                                                )
+                                            )
+                                        },
+                                        caretSize = DpSize(27.dp, 17.dp),
+                                        colors = RichTooltipColors(
+                                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                                            contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                            titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                                            actionContentColor = MaterialTheme.colorScheme.onTertiaryContainer
+                                        )
+                                    ) {
+                                        Text("Code will be copied as markdown codeblock")
+                                    }
+                                },
+                                state = rememberTooltipState()
+                            ){
+                                IconButton(onClick = {viewModel.copyCodeAsMarkdown(openedSnippet)}){
+                                    Icon(
+                                        painterResource(Res.drawable.square_m),
+                                        contentDescription = "Copy code as a markdown codeblock"
+                                    )
+                                }
+                            }
+
+                        }
                     }
                 }
                 Box(
