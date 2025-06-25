a864a1032a32e167adb4b5a580ddb7b811be7110
zahid4kh <halilzahid@gmail.com>
2025-06-25 13:39:16 +0200

    SnippetCard.kt -> added dropdown menu for exporting as txt and md

--- src/main/kotlin/screens/SnippetCard.kt
+++ src/main/kotlin/screens/SnippetCard.kt
@@ -1,11 +1,12 @@
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -16,14 +17,20 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kached.resources.*
 import org.jetbrains.compose.resources.painterResource
+import theme.getJetbrainsMonoFamily
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SnippetCard(
     snippet: Snippet,
     onDelete: (String) -> Unit,
     modifier: Modifier,
     onMaximizeSnippet: () -> Unit = {},
+    onExportAsText: () -> Unit = {},
+    onExportAsMarkdown: () -> Unit = {}
 ) {
+    var dropdownExpanded by remember { mutableStateOf(false) }
+
     OutlinedCard(
         modifier = modifier
             .fillMaxWidth()
@@ -46,6 +53,56 @@ fun SnippetCard(
                     color = MaterialTheme.colorScheme.primary,
                     modifier = Modifier.weight(1f)
                 )
+                Box{
+                    TooltipBox(
+                        tooltip = {PlainTooltip { Text("Export as") }},
+                        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+                        state = rememberTooltipState()
+                    ){
+                        IconButton(
+                            onClick = {dropdownExpanded = !dropdownExpanded}
+                        ){
+                            Icon(
+                                painter = painterResource(Res.drawable.download),
+                                contentDescription = "Export as"
+                            )
+                        }
+                    }
+
+                    DropdownMenu(
+                        expanded = dropdownExpanded,
+                        onDismissRequest = {dropdownExpanded = false},
+                        shape = MaterialTheme.shapes.medium,
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline)
+                    ){
+                        DropdownMenuItem(
+                            text = {Text("Export as .txt")},
+                            leadingIcon = {
+                                Icon(
+                                    painterResource(Res.drawable.text),
+                                    contentDescription = "Export as .txt",
+                                    modifier = Modifier.size(24.dp),
+                                    tint = MaterialTheme.colorScheme.onSurface
+                                )
+                            },
+                            onClick = {onExportAsText()}
+                        )
+                        DropdownMenuItem(
+                            text = {Text("Export as .md")},
+                            leadingIcon = {
+                                Icon(
+                                    painterResource(Res.drawable.markdown),
+                                    contentDescription = "Export as .md",
+                                    modifier = Modifier.size(24.dp),
+                                    tint = MaterialTheme.colorScheme.onSurface
+                                )
+                            },
+                            onClick = {onExportAsMarkdown()}
+                        )
+                    }
+                }
+
                 IconButton(
                     onClick = {
                         onMaximizeSnippet()
@@ -63,7 +120,11 @@ fun SnippetCard(
                     Icon(Icons.Default.Delete, contentDescription = "Delete Snippet", tint = MaterialTheme.colorScheme.error)
                 }
             }
+
+
             Spacer(Modifier.height(8.dp))
+
+
             snippet.description?.takeIf { it.isNotBlank() }?.let {
                 Text(
                     text = it,
@@ -74,11 +135,13 @@ fun SnippetCard(
                 )
                 Spacer(Modifier.height(8.dp))
             }
+
+
             snippet.code?.takeIf { it.isNotBlank() }?.let {
                 Text(
                     text = it,
                     style = MaterialTheme.typography.bodyMedium.copy(fontSize = 12.sp),
-                    fontFamily = FontFamily.Monospace,
+                    fontFamily = getJetbrainsMonoFamily(),
                     color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
                     modifier = Modifier
                         .fillMaxWidth()
